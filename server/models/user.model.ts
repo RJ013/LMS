6d987e79c40f2e48eb691acd24d9c7427a45e99c
Variant25 <ayushp041@gmail.com>
2024-08-28 21:10:42 +0530

    User login, logout

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,5 +1,7 @@
+require("dotenv").config();
 import mongoose, { Document, Model, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 const emailRegexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,6 +17,8 @@ export interface IUser extends Document{
     isVerified:boolean;
     courses:Array<{courseId: string}>;
     comparePassword:(password:string)=> Promise<boolean>;
+    SignAccessToken: ()=> string;
+    SignRefreshToken: ()=> string;
     
 };
 
@@ -71,6 +75,18 @@ const userSchema: Schema<IUser> = new mongoose.Schema({
         this.password = await bcrypt.hash(this.password,10);
         next();
     });
+
+
+    //sign access token
+
+    userSchema.methods.SignAccessToken = function(){
+        return jwt.sign({id: this._id}, process.env.ACCESS_TOKEN || '');
+    };
+
+    //sign refresh token
+    userSchema.methods.SignRefreshToken = function(){
+        return jwt.sign({id: this._id}, process.env.REFRESH_TOKEN || '');
+    };
 
     //compare password
 

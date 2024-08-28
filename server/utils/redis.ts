f69933f89ad3bd5550a615b901c31f01b04d6c3f
Rohit jaiswal <rohitjaiswalself@gmail.com>
2024-08-29 00:01:44 +0530

    redis

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -19,3 +19,18 @@ const redisClient = () => {
 };
 
 export const redis = redisClient();
+
+
+
+// import {Redis} from 'ioredis';
+// require('dotenv').config(); 
+
+// const redisClient = () => {
+//     if(process.env.REDIS_URL){
+//         console.log('Redis connected');
+//         return process.env.REDIS_URL;
+//     }
+//     throw new Error('Redis connection failed');
+// };
+
+// export const redis = new Redis(redisClient());

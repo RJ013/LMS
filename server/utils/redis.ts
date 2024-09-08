a70b4a4bd5b370e7dabe35999e5f08df3695bba9
Rohit Jaiswal <rohitjaiswalself@gmail.com>
2024-09-08 13:41:44 +0530

    redis

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,36 +1,14 @@
-import { Redis } from 'ioredis';
+
+
+import {Redis} from 'ioredis';
 require('dotenv').config(); 
 
 const redisClient = () => {
-    const redisHost = process.env.REDIS_HOST || '127.0.0.1'; // Default to localhost
-    const redisPort = process.env.REDIS_PORT || 6379; // Default Redis port
-
-    try {
-         
-        const redis = new Redis({
-            host: redisHost,
-            port: redisPort,
-        });
-        console.log('Redis connected locally');
-        return redis;
-    } catch (error:any) {
-        throw new Error('Redis connection failed: ' + error.message);
+    if(process.env.REDIS_URL){
+        console.log('Redis connected');
+        return process.env.REDIS_URL;
     }
+    throw new Error('Redis connection failed');
 };
 
-export const redis = redisClient();
-
-
-
-// import {Redis} from 'ioredis';
-// require('dotenv').config(); 
-
-// const redisClient = () => {
-//     if(process.env.REDIS_URL){
-//         console.log('Redis connected');
-//         return process.env.REDIS_URL;
-//     }
-//     throw new Error('Redis connection failed');
-// };
-
-// export const redis = new Redis(redisClient());
+export const redis = new Redis(redisClient());

775e12b2337b80f54e6b8f1934de4a75fa7f4391
Variant25 <ayushp041@gmail.com>
2024-10-03 19:37:08 +0530

    layout api added

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { authorizeRoles,isAuthenticated } from "../middleware/auth";
 
 import { updateAccessToken } from "../controllers/user.controller";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 layoutRouter.post("/create-layout",updateAccessToken,isAuthenticated,authorizeRoles("admin"),createLayout);
+layoutRouter.put("/edit-layout",updateAccessToken,isAuthenticated,authorizeRoles("admin"),editLayout);
+layoutRouter.get("/get-layout/:type",getLayoutByType)
 export default layoutRouter;
--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,19 +1,35 @@
 
-
-import {Redis} from 'ioredis';
-require('dotenv').config(); 
+import { Redis } from 'ioredis';
+require('dotenv').config();
 
 const redisClient = () => {
     if (process.env.REDIS_URL) {
-        console.log('Redis connected');
-        return new Redis(process.env.REDIS_URL, {
+        console.log('Attempting Redis connection...');
+        const client = new Redis(process.env.REDIS_URL, {
             retryStrategy(times) {
                 const delay = Math.min(times * 50, 2000); // Delay retry with backoff strategy
+                console.log(`Redis connection attempt ${times}. Retrying in ${delay} ms...`);
                 return delay;
-            }
+            },
+            connectTimeout: 10000, // 10 seconds timeout for connection attempts
         });
+
+        client.on('connect', () => {
+            console.log('Redis successfully connected.');
+        });
+
+        client.on('error', (err) => {
+            console.error('Redis connection error:', err);
+        });
+
+        client.on('end', () => {
+            console.log('Redis connection closed.');
+        });
+
+        return client;
     }
-    throw new Error('Redis connection failed');
+    
+    throw new Error('Redis connection failed: REDIS_URL not provided.');
 };
 
 export const redis = redisClient();
